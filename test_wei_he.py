faac40758b06a07d08976ef403a8d9adda4633de
Weimender <wei.he@telecom-paristech.fr>
2014-10-09 21:37:20 +0200

    Test

--- test_wei_he.py
+++ test_wei_he.py
@@ -1,1 +1,12 @@
 print "Hello World"
+
+print "TOTOTOTO"
+
+test = []
+test.append('qqqqqqqqqqqqq')
+test.append('oewihoiewr')
+
+test2 = []
+test2.append('23128371289')
+test2.append('hhhhhhhhhhhhhhh')
+
