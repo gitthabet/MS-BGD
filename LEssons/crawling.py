db2c349fd2fe6b7c5b1af9b2e4e3abf556153d35
Charles Miglietti <charles.miglietti@yahoo.fr>
2014-10-14 10:58:41 +0200

    better version with refactor

--- LEssons/crawling.py
+++ LEssons/crawling.py
@@ -2,23 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 
-result = requests.get('https://www.youtube.com/results?search_query=rihanna')
-
-
-if result.status_code == 200:
-    print 'Request succesful'
-    soup = BeautifulSoup(result.text)
-    balises_a = soup.find_all("a", class_="yt-uix-tile-link")
-    links = [balise.get('href') for balise in balises_a]
-    print 'Here are the links', links
-    links.pop(0)
-    link = links[0]
-    pageHTML = requests.get('https://www.youtube.com/'+link).text
+# Returns a soup object from a given url
+def getSoupFromUrl(url):
+    result = requests.get(url)
     if result.status_code == 200:
-        pageSoup = BeautifulSoup(pageHTML)
-        print pageSoup.title
+        print 'Request succesful'
+        return BeautifulSoup(result.text)
     else:
-        print 'Request for page failed', link
+        print 'Request failed', url
+        return None
 
-else:
-    print 'Request failed'
+soupYoutube = getSoupFromUrl('https://www.youtube.com/results?search_query=rihanna')
+balises_a = soupYoutube.find_all("a", class_="yt-uix-tile-link")
+links = [balise.get('href') for balise in balises_a]
+print 'Here are the links', links
+links.pop(0)
+link = links[0]
+soupPage = getSoupFromUrl('https://www.youtube.com' +link)
+print soupPage.title
