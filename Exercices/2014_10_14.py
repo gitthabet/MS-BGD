f122606332ddb85e096afc96220d83957bf67074
guillaumeph <guillaume.perrin-houdon@telecom-paristech.fr>
2014-10-14 09:08:01 +0200

    guillaumeph/

--- Exercices/2014_10_14.py
+++ Exercices/2014_10_14.py
@@ -5,12 +5,22 @@ import unittest
 # that is n copies of the original string.
 
 def string_times(string, n):
-    return
+    result=""  
+    for count in range (0,n):
+        result = result+string
+        count = count+1
+    return result
+
 
 # Given an array of ints, return True if one of the first 4 elements
 # in the array is a 9. The array length may be less than 4.
 def array_front9(nums):
-    return
+    def is_true(x):
+        if x==9:
+            return True
+    nums_2 = filter(is_true, nums)
+    if len(nums_) == 4:
+        return True
 
 # Given a string, return the count of the number of times
 # that a substring length 2 appears  in the string and also as
