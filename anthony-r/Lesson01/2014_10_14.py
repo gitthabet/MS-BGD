3d28e546da9a98b877e55f4a34eebe2da92ed9e1
anthonyray <dantho361@hotmail.com>
2014-10-17 16:10:46 +0200

    Refactoring folder structure

--- anthony-r/Lesson01/2014_10_14.py
+++ anthony-r/Lesson01/2014_10_14.py
@@ -23,7 +23,19 @@ def array_front9(nums):
 # that a substring length 2 appears  in the string and also as
 # the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
 def last2(string):
-    return
+    if len(string) < 2:
+      return 0
+
+    last2 = str[len(string)-2:]
+    count = 0
+
+    # Check each substring length 2 starting at i
+    for i in range(len(string)-2):
+      sub = str[i:i+2]
+      if sub == last2:
+        count = count + 1
+
+    return count
 
 
 # Here's our "unit tests".
