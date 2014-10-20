e289d193e6435074d6599d4ed7f2eae9c372790a
githicham <githicham>
2014-10-20 23:25:20 +0200

    upd annee

--- hicham-h/Lesson2/exo_dom_lesson_02.py
+++ hicham-h/Lesson2/exo_dom_lesson_02.py
@@ -4,6 +4,14 @@ from bs4 import BeautifulSoup
 import re
 import sys
 
+#Fonction de téléchargement
+def get_page(year):
+    response = requests.get("http://alize2.finances.gouv.fr/communes/eneuro/detail.php?icom=056&dep=075&type=BPS&param=5&exercice="+str(year))
+    if response.status_code == 200:
+        return BeautifulSoup(response.text)
+    else:
+        return None
+
 #Fonction d'extraction du montant par habitant
 def extract_data (tableau, data):
     lignes = tableau.find_all("tr")
@@ -15,7 +23,7 @@ def extract_data (tableau, data):
             if td != None:
 #               Ligne "Total" trouvée               
                 ligne_trouvee = True
-                print td.text + " -> " + td.parent.select("td:nth-of-type(2)")[0].text + "€/hab"
+                print td.text + " -> " + td.parent.select("td:nth-of-type(2)")[0].text + "eur/hab"
             else:
                 continue
         else:
@@ -25,31 +33,33 @@ def extract_data (tableau, data):
             else:
                 td = ligne.find('td',attrs={'class':re.compile('libellepetit(.*)', re.IGNORECASE)})
                 if td != None:
-                    print td.text + " -> " + td.parent.select("td:nth-of-type(2)")[0].text + "€/hab"
+                    print td.text + " -> " + td.parent.select("td:nth-of-type(2)")[0].text + "eur/hab"
 
-#Téléchargement des données
-response = requests.get("http://alize2.finances.gouv.fr/communes/eneuro/detail.php?icom=056&dep=075&type=BPS&param=5&exercice=2013")
-if response.status_code == 200:
-   bs = BeautifulSoup(response.text)
-else:
-   sys.exit("Request failed")
+for i in [2009,2010,2011,2012,2013]:
 
-#On extrait le tableau qui nous intéresse
-tableau = None
-tables = bs.find_all("table")
-for table in tables:
-    td = table.find("td",text=re.compile("ANALYSE(.*)FONDAMENTAUX", re.IGNORECASE))
-    if td != None:
-        tableau = table
-        break
+#   On télécharge les données
+    print ""
+    print "DONNEES DE L'ANNEE " + str(i)
+    bs = get_page(i)
+    if bs == None:
+        sys.exit("Request failed")
 
-#On extrait les données du tableau
-if tableau != None:
-    extract_data(tableau, "A")
-    extract_data(tableau, "B")
-    extract_data(tableau, "C")    
-    extract_data(tableau, "D")  
-else:
-    print "Tableau non trouvé"
+#   On extrait le tableau qui nous intéresse
+    tableau = None
+    tables = bs.find_all("table")
+    for table in tables:
+        td = table.find("td",text=re.compile("ANALYSE(.*)FONDAMENTAUX", re.IGNORECASE))
+        if td != None:
+            tableau = table
+            break
+
+#    On extrait les données du tableau
+    if tableau != None:
+        extract_data(tableau, "A")
+        extract_data(tableau, "B")
+        extract_data(tableau, "C")    
+        extract_data(tableau, "D")  
+    else:
+        print "Tableau non trouvé"
 
             
