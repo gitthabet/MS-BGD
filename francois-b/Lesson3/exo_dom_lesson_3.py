c2eb2144e81c5e92518d738c7062e227e4955ca7
franblas <francois_blas@hotmail.com>
2014-10-26 11:13:22 +0100

    exo_dom_git_crawl_final

--- francois-b/Lesson3/exo_dom_lesson_3.py
+++ francois-b/Lesson3/exo_dom_lesson_3.py
@@ -30,6 +30,7 @@ p = 0
 nb_contrib = 1
 max_contrib = 256
 offset = 2
+user_stars = [("",0.0)]
 
 # Page to crawl
 r = req.get('https://gist.github.com/paulmillr/2657075')
@@ -38,14 +39,16 @@ soup = bs(r.text)
 for link in soup.find_all('a', attrs={'rel' : 'noreferrer'}):
     if link.get('href').startswith("https://github.com/") and link.get('href').count('/')==3:
         if(p>offset and p<max_contrib+offset): 
-            user = link.get('href').split('/')[3]
-            print user        
+            user = link.get('href').split('/')[3]     
             sum = 0.0
             rr = req.get("https://api.github.com/users/"+user+"/repos")
             rep = rr.json()
             for count in range(0,len(rep)):
                 sum = sum + rep[count]['stargazers_count']
-            print "Stars average: "+str(sum/len(rep))    
+            # fill list with tuple (user,average)
+            user_stars.append((user,sum/len(rep)))
             nb_contrib = nb_contrib + 1
         p = p + 1
-print nb_contrib # verify that nb_contrib = 256
+# sort by average desc        
+user_stars.sort(key=lambda tup: tup[1],reverse=True)    
+print user_stars
