ef410ff1dd2fd3cf2d533f1c1bdf327ff9ff9e1e
Anthony Hayot <anthony@anthony-VirtualBox.(none)>
2014-10-27 10:08:09 +0100

    exo 03

--- anthony-h/Lesson03/exo_class_lesson03.py
+++ anthony-h/Lesson03/exo_class_lesson03.py
@@ -2,18 +2,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='https://news.ycombinator.com/'
-result=requests.get(url)
-soup=BeautifulSoup(result.text)
-balises_a = soup.find_all("a",id)
 
-links = [balise.get('href') for balise in balises_a]
-i=0
-users=[]
-for link in links:
-	#print link[0:7]
-	if link[0:7]=='user?id':
-		users.append(link.replace('user?id=',""))
-	i=i+1
+def getUser(page):
+	url='https://news.ycombinator.com//news?p=',page
+	result=requests.get(url)
+	soup=BeautifulSoup(result.text)
+	balises_a = soup.find_all("a",id)
 
-print users
+	links = [balise.get('href') for balise in balises_a]
+	i=0
+	users=[]
+	for link in links:
+		#print link[0:7]
+		if link[0:7]=='user?id':
+			users.append(link.replace('user?id=',""))
+		i=i+1
+
+	return users
+
+for page in [1,2,3]:
+	user=getUser(page)
+	print user
