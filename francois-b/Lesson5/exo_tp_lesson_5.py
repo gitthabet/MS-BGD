0d22795fa078d44693269eedd5a5370e72f43652
franblas <francois_blas@hotmail.com>
2014-11-04 09:48:18 +0100

    tp5

---
+++ francois-b/Lesson5/exo_tp_lesson_5.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 04 08:41:35 2014
+
+@author: Paco
+"""
+
+'''
+Pour Levothyroxine
+Liste des medicaments rembourses
+Dosage + Unite + Forme
+'''
+
+import requests as req
+from bs4 import BeautifulSoup as bs
+import re
+
+res = []
+
+## post
+payload = {'page': '1', 'btnMedic.x': '12', 'btnMedic.y': '7', 'choixRecherche': 'medicament', 'txtCaracteres': 'Levothyroxine', 'btnMedic': 'Rechercher'}
+r = req.post("http://base-donnees-publique.medicaments.gouv.fr/index.php", data=payload)
+soup = bs(r.text)
+
+## get links
+for link in soup.find_all('a', attrs={'class' : 'standart'}):
+    
+    title = link.get('title').split(':')[1].strip()
+    # get name    
+    name = title.split(',')[0]
+    name1 = str(name.split()[0])
+    name2 = str(name.split()[1])    
+    dosage = int(name.split()[2])
+    unite = str(name.split()[3])
+    # get forme    
+    galetruc = str(title.split(',')[1].strip()) 
+        
+    # link for infos    
+    url = "http://base-donnees-publique.medicaments.gouv.fr/"+link.get('href')
+    
+    rr = req.get(url)
+    soupp = bs(rr.text)
+
+    # get price and re        
+    remboursement = soupp.find('div', attrs={'class' : 'infosCnam'})  
+    rembou = remboursement.text.split()    
+    prix = str(rembou[2])   
+    tauxrembou = str(rembou[8])    
+    
+    # get autorization
+    autorization = soupp.find('div', attrs={'class' : 'alignright'})   
+    auto = autorization.text.split()[4]
+
+    # get comm
+    #comm = re.search('(.*)commercialisation : (.*)\"', rr.text)   
+    
+    res.append((name1+" "+name2,dosage,unite,galetruc,prix,tauxrembou+" %",auto))
+    
+    print res
