a415391005fa3a61f16669905b0982642d5f5161
gitthabet <tchelligue@gmail.com>
2014-12-15 02:13:06 +0100

    update karma

---
+++ thabet_c/Lesson3/exo_cours_karma.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 14 22:55:25 2014
+
+@author: thabetchelligue
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+def getSoupFromUrl(url):
+    results=requests.get(url)
+    soup=BeautifulSoup(results.text)
+    if results.status_code==200:
+
+        return soup
+    else:
+        print 'Request failed'
+    return None
+    
+
+def getUserFromYcombinator():
+    url='https://news.ycombinator.com/'
+    soupY=getSoupFromUrl(url)
+    balises_td=soupY.find_all('td',class_="subtext")
+    users=[]
+    for balise in balises_td:
+        info_user=balise.text.split()
+        print info_user
+        if info_user[2]=="by":
+            user=info_user[3]
+            users.append(user)
+    print "users are :"+str(users)        
+    users.sort()
+    return users
+
+def getKarmaFromUser():
+    url='https://news.ycombinator.com/'
+    users=getUserFromYcombinator()
+    for user in users:
+        SoupKarma=getSoupFromUrl(url+'user?id='+user)
+        karmalevel=SoupKarma.text.find("karma").split()
+        karma=karmalevel[0]
+        print karma+user
+
